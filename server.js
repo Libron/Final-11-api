671605a6f25adffd537df03f793f6804e2eece54
Urmat Bekboev <bek.urmat@gmail.com>
2019-04-27 17:15:08 +0600

    users, categories, products routes added

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
-console.log();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./config');
 
+const users = require('./app/users');
+const categories = require('./app/categories');
+const products = require('./app/products');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const port = 8000;
+
+mongoose.connect(config.dbUrl, config.mongoOptions).then(() => {
+    app.use('/categories', categories);
+    app.use('/users', users);
+    app.use('/products', products);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port`);
+    });
+});
